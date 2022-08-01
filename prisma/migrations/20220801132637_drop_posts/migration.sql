319e33f0921eeeb0a3cde1625cb905655690b389
Alec Winograd <alec@ouitherapeutics.com>
2022-08-01 08:26:41 -0500

    remove posts / extraneous columns

---
+++ prisma/migrations/20220801132637_drop_posts/migration.sql
@@ -0,0 +1,20 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `email` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `name` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the `Post` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Post" DROP CONSTRAINT "Post_authorId_fkey";
+
+-- DropIndex
+DROP INDEX "User.email_index";
+
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "email",
+DROP COLUMN "name";
+
+-- DropTable
+DROP TABLE "Post";
